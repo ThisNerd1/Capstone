7c9941138829b584f29ab44d45936fa4bac29145
Daijah <daijahlinyahcoppage@gmail.com>
2023-04-04 13:38:57 -0600

    setting up project

---
+++ noted/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Home from './pages/Home';
+import Lists from './pages/Lists';
+
+function App() {
+  return (
+    <div className="App">
+      see me bro
+    </div>
+  );
+}
+
+export default App;
---
+++ noted/src/pages/Lists.js
@@ -0,0 +1,7 @@
+const Lists = () => {
+    return (
+        <h1>Welcome to Noted's list page!</h1>
+    );
+}
+
+export default Lists;
