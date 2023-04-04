af401c65b2719f9a5570265fa98fce8e021baa63
Daijah <daijahlinyahcoppage@gmail.com>
2023-04-04 14:42:20 -0600

    routes

--- noted/src/App.js
+++ noted/src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Lists from './pages/Lists';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      see me bro
+      <h1>Welcome to Noted</h1>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/gifts" element={<Lists />} />
+      </Routes>
     </div>
+    </div>
+    </BrowserRouter>
   );
 }
 
